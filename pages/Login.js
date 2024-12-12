8b78abd810c03a6ff638a36cba548b6edfa5a9be
ZaeemRizwan2003 <210657@students.au.edu.pk>
2024-12-12 19:44:03 +0500

    integrated driver login with other login page

--- pages/Login.js
+++ pages/Login.js
@@ -1,116 +1,131 @@
-import React, { useState } from 'react';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import axios from 'axios';
+import React, { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import axios from "axios";
 
 const Login = () => {
-    const router = useRouter();
-    const [formData, setFormData] = useState({ email: '', password: '' });
-    const [error, setError] = useState('');
-    const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const [formData, setFormData] = useState({ identifier: "", password: "" });
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setError('');
-        setLoading(true);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    setLoading(true);
 
-        try {
-            const res = await axios.post('/api/login', {
-                email: formData.email,
-                password: formData.password,
-            });
+    try {
+      const res = await axios.post("/api/login", formData);
 
-            if (res.status === 200) {
-                const { token, userId, userType } = res.data;
+      if (res.status === 200) {
+        const { token, userId, userType } = res.data;
 
-                // Store the token and userId in localStorage or cookies as needed
-                localStorage.setItem('token', token);
-                localStorage.setItem('userId', userId);
+        localStorage.setItem("token", token);
+        localStorage.setItem("userId", userId);
 
-                // Redirect based on userType
-                if (userType === 'bakery') {
-                    router.push('/Restaurants/RDashboard'); // Redirect to Registered Bakery Dashboard
-                } else if (userType === 'listing') {
-                    router.push('/Customer/Cdashboard'); // Redirect to Listing User Dashboard
-                } else if (userType === 'delivery') {
-                    router.push('/Delivery/deliverydashboard'); // Redirect to Delivery Partner Dashboard
-                }
-            }
-        } catch (err) {
-            setError('Invalid email or password. Please try again.');
-            setLoading(false); // Stop loading if login fails
+        if (userType === "bakery") {
+          router.push("/Restaurants/RDashboard");
+        } else if (userType === "listing") {
+          router.push("/Customer/Cdashboard");
+        } else if (userType === "delivery") {
+          localStorage.setItem("delivery", JSON.stringify(formData));
+
+          router.push("/Delivery/deliverydashboard");
         }
-    };
+      }
+    } catch (err) {
+      setError("Invalid email, phone number, or password. Please try again.");
+      setLoading(false);
+    }
+  };
 
-    return (
-        <section className="bg-gray-50 ">
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <div className="sm:mx-auto sm:w-full sm:max-w-sm mb-3">
-                    <img
-                        className="mx-auto h-10 w-auto"
-                        src="/mainlogo.png"
-                        alt="EndofDay"
-                    />
-                </div>
-                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 ">
-                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                            Log in to your account
-                        </h1>
-                        {error && <p className="text-red-500 text-center">{error}</p>}
-                        <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
-                            <div>
-                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-                                <input
-                                    type="email"
-                                    name="email"
-                                    id="email"
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                                    placeholder="name@company.com"
-                                    value={formData.email}
-                                    onChange={handleChange}
-                                    required
-                                />
-                            </div>
-                            <div>
-                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
-                                <input
-                                    type="password"
-                                    name="password"
-                                    id="password"
-                                    placeholder="••••••••"
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                                    value={formData.password}
-                                    onChange={handleChange}
-                                    required
-                                />
-                            </div>
-                            <div className="flex items-center justify-between">
-                                <Link href="/ForgotPassword/Forgot-Password" className="text-sm font-medium text-primary-600 hover:underline text-black">Forgot password?</Link>
-                            </div>
-                            <button
-                                type="submit"
-                                className="w-full text-black bg-purple-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                                disabled={loading}
-                            >
-                                {loading ? 'Signing in...' : 'Sign in'}
-                            </button>
-                            <p className="text-sm text-black font-light">
-                                Don’t have an account yet?{' '}
-                                <Link href="/Restaurants/Signup" className="font-medium text-black hover:underline">
-                                    Sign up
-                                </Link>
-                            </p>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className="bg-gray-50">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm mb-3">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="/mainlogo.png"
+            alt="EndofDay"
+          />
+        </div>
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+              Log in to your account
+            </h1>
+            {error && <p className="text-red-500 text-center">{error}</p>}
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+              <div>
+                <label
+                  htmlFor="identifier"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Email or Phone Number
+                </label>
+                <input
+                  type="text"
+                  name="identifier"
+                  id="identifier"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  placeholder="Enter email or phone number"
+                  value={formData.identifier}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  value={formData.password}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <Link
+                  href="/ForgotPassword/Forgot-Password"
+                  className="text-sm font-medium text-primary-600 hover:underline text-black"
+                >
+                  Forgot password?
+                </Link>
+              </div>
+              <button
+                type="submit"
+                className="w-full text-black bg-purple-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                disabled={loading}
+              >
+                {loading ? "Signing in..." : "Sign in"}
+              </button>
+              <p className="text-sm text-black font-light">
+                Don’t have an account yet?{" "}
+                <Link
+                  href="/Restaurants/Signup"
+                  className="font-medium text-black hover:underline"
+                >
+                  Sign up
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
